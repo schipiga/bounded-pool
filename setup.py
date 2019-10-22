5dae8a0b80c0c5e93b96d749782875518a82684b
Sergei Chipiga <sergei.chipiga@pipedrive.com>
2019-10-22 20:32:01 +0300

    Make pool release future dependant

--- setup.py
+++ setup.py
@@ -9,5 +9,7 @@ setup(
     author='Sergei Chipiga <chipiga86@gmail.com>',
     author_email='chipiga86@gmail.com',
     py_modules=['bounded_pool'],
-    install_requires=['Pebble==4.4.0'],
+    install_requires=[
+        'Pebble==4.4.0',
+        'multilock @ git+https://github.com/schipiga/multilock@v0.1'],
 )
